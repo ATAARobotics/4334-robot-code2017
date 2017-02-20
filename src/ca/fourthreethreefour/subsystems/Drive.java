1f7e17a1717057ee611165a82679e8be7fb3520a
Trevor Tsang <Trevor Tsang>
2017-02-20 10:07:11 -0700

    Drivetrain drove backwards, now it doesn't

--- src/ca/fourthreethreefour/subsystems/Drive.java
+++ src/ca/fourthreethreefour/subsystems/Drive.java
@@ -1,5 +1,6 @@
 package ca.fourthreethreefour.subsystems;
 
+import edu.first.identifiers.InversedSpeedController;
 import edu.first.module.Module;
 import edu.first.module.actuators.Drivetrain;
 import edu.first.module.actuators.VictorModule;
@@ -9,18 +10,18 @@ import edu.first.module.subsystems.Subsystem;
 public interface Drive extends Settings {
 	
 	VictorModuleGroup left = new VictorModuleGroup(new VictorModule[] { 
-			new VictorModule(0), //TODO get ports from external file
-			new VictorModule(1),
-			new VictorModule(2)
-	});
-	
-	VictorModuleGroup right = new VictorModuleGroup(new VictorModule[] { 
-			new VictorModule(3),
+			new VictorModule(3), //TODO get ports from external file
 			new VictorModule(4),
 			new VictorModule(5)
 	});
+	
+	VictorModuleGroup right = new VictorModuleGroup(new VictorModule[] { 
+			new VictorModule(0),
+			new VictorModule(1),
+			new VictorModule(2)
+	});
 
-	Drivetrain drivetrain = new Drivetrain(left, right);
+	Drivetrain drivetrain = new Drivetrain(new InversedSpeedController(left), right);
 
 	Subsystem drive = new Subsystem(new Module[] {
 			drivetrain, left, right

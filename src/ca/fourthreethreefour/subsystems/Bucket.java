731fcdfccd8316d2a301ecafedb9ccb2c01cf273
Trevor Tsang <Trevor Tsang>
2017-03-30 12:40:02 -0600

    groundIntake is a TalonModule on practice bot

--- src/ca/fourthreethreefour/subsystems/Bucket.java
+++ src/ca/fourthreethreefour/subsystems/Bucket.java
@@ -5,6 +5,7 @@ import edu.first.module.Module;
 import edu.first.module.actuators.DualActionSolenoid;
 import edu.first.module.actuators.DualActionSolenoid.Direction;
 import edu.first.module.actuators.DualActionSolenoidModule;
+import edu.first.module.actuators.TalonModule;
 import edu.first.module.actuators.VictorModule;
 import edu.first.module.sensors.DigitalInput;
 import edu.first.module.subsystems.Subsystem;
@@ -17,7 +18,7 @@ public interface Bucket extends Settings {
 
     Subsystem bucket = new Subsystem(new Module[] { bucketSolenoid });
     
-    VictorModule groundIntake = new VictorModule(GROUND_INTAKE);
+    TalonModule groundIntake = new TalonModule(GROUND_INTAKE);
     
     DualActionSolenoid.Direction BUCKET_IN = Direction.LEFT;
     DualActionSolenoid.Direction BUCKET_OUT = Direction.RIGHT;
